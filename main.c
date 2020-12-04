ee5260134c0c86bf77cd0fc95b2f6d4fbe78d4af
RazMag <razmagorii@gmail.com>
2020-12-04 14:48:16 +0200

    reformated the boardsize to boardDimensions struct, looks better. might lose me some grade score we'll see.

--- main.c
+++ main.c
@@ -7,26 +7,21 @@
 #define MAXROWS 23
 #define MAXCOLUMNS 23
 
-void requestGameVariation(int[], int);
 
-void requestCustomBoard(int[], int);
+typedef struct boardDimensionsStruct { // Struct used to refer to the size of the board that is played on.
+    int width;
+    int height;
+} boardDimensionsType;
 
-void runGame(int[], int);
-
-void plantMines(char[MAXCOLUMNS][MAXROWS], int, int[]);
-
-struct coordinate generateMineCoordinate(int[]);
-
+boardDimensionsType requestGameVariation(boardDimensionsType);
+boardDimensionsType requestCustomBoard(boardDimensionsType);
+void runGame(boardDimensionsType);
+void plantMines(char[MAXCOLUMNS][MAXROWS], int, boardDimensionsType boardDimensions);
+struct coordinate generateMineCoordinate(boardDimensionsType boardDimensions);
 void clearBoard(char[MAXCOLUMNS][MAXROWS]);
-
 struct coordinate {
     int column;
     int row;
-};
-
-struct boardSize {
-    int width;
-    int height;
 };
 
 //TODO things to consider,
@@ -38,16 +33,16 @@ void main() {
     /*TODO ADD "README"*/
     setbuf(stdout, 0); //TODO REMOVE THIS, ONLY HERE TO FIX CLION'S BUFFER BUG
     srand(time(NULL)); /*seed the random function for future use*/
-    int boardSize[2]; /*to be used for board size in [height,width] format. WILL ALWAYS BE OF SIZE 2. GAME BREAKS WHEN GOING 3D*/
-    requestGameVariation(boardSize, 2);
-    if (boardSize[0] != 0) /*check for no game signal - boardsize [0,0]*/
+    boardDimensionsType boardDimensions; /*to be used for board size in [height,width] format. WILL ALWAYS BE OF SIZE 2. GAME BREAKS WHEN GOING 3D*/
+    boardDimensions = requestGameVariation(boardDimensions);
+    if (boardDimensions.height != 0) /*check for no game signal - boardDimensions.height = 0 */
     {
-        runGame(boardSize, 2);
+        runGame(boardDimensions); //TODO FIX
     }
-    printf("height = %d\nwidth = %d", boardSize[0], boardSize[1]); //TODO REMOVE - HERE FOR DEBUGGING
+    printf("height = %d\nwidth = %d", boardDimensions.height, boardDimensions.width); //TODO REMOVE - HERE FOR DEBUGGING
 }
 
-void requestGameVariation(int boardSize[], int arraySize) {
+boardDimensionsType requestGameVariation(boardDimensionsType boardDimensions) {
     //TODO ADD READ ME
     /*print game variations*/
     int menuChoice = 0; /*to be used for the players choice in the menu*/
@@ -62,25 +57,24 @@ void requestGameVariation(int boardSize[], int arraySize) {
         scanf("%d", &menuChoice);
         switch (menuChoice) {
             case 0:
-                boardSize[0] = boardSize[1] = 0;/*exit requested, set board size to 0 to signal "no game"*/
+                boardDimensions.width = boardDimensions.height = 0;/*exit requested, set board size to 0 to signal "no game"*/
                 boardSizeSetFlag = 'T'; /*exit requested set flag to T to exit while loop*/
                 break;
             case 1:
-                boardSize[0] = 8; /*board size of 8X8*/
-                boardSize[1] = 8; /*board size of 8X8*/
+                boardDimensions.width = boardDimensions.height = 8; /*board size of 8X8*/
                 boardSizeSetFlag = 'T'; /*board size is decided upon set flag to T to exit while loop*/
                 break;
             case 2: /*# NOT USED IN PART A#*/
-//                boardSize[0]=boardSize[1]=12; /*board size of 12X12*/
+//                boardDimensions.width = boardDimensions.height = 12; /*board size of 12X12*/
 //                boardSizeSetFlag = 'T'; /*board size is decided upon set flag to T to exit while loop*/
                 break;
             case 3: /*# NOT USED IN PART A#*/
-//                boardSize[0]=boardSize[1]=15; /*board size of 12X12*/
+//                boardDimensions.width = boardDimensions.height =15; /*board size of 12X12*/
 //                boardSizeSetFlag = 'T'; /*board size is decided upon set flag to T to exit while loop*/
                 break;
             case 4: /*# NOT USED IN PART A#*/
-//                requestCustomBoard(boardSize, arraySize); /*set board size in custom board function*/
-//                if(boardSize[0]!=0){
+//                boardDimensions = requestCustomBoard(boardDimensions, arraySize); /*set board size in custom board function*/
+//                if(boardDimensions.height!=0){
 //                    boardSizeSetFlag = 'T'; /*board size is decided upon set flag to T to exit while loop*/
 //                }
                 break;
@@ -89,38 +83,38 @@ void requestGameVariation(int boardSize[], int arraySize) {
                 break;
         }
     }
+    return boardDimensions;
 }
 
-void requestCustomBoard(int boardSize[], int arraySize) {
-    /* receives boardsize array and its size, requests input from user for custom board size
+boardDimensionsType requestCustomBoard(boardDimensionsType boardDimensions) {
+    /* receives boardDimensions struct, requests input from user for custom board size
      * makes sure inpyt is valid (width and height between 1 and 23)
-     * set board size array according to input
+     * set board size struct according to input
      * sets size to 0X0 on invalid input*/
     /*#WILL NOT RUN IN PART A#*/
     int boardHeight = 0, boardWidth = 0;
     printf("Please enter the size of the board (Lines <= 22 and Cols <= 22 ):");
     scanf("%d %d", &boardHeight, &boardWidth);
-    if (boardHeight < 23 || boardWidth < 23 || boardHeight < 1 ||
-        boardWidth < 1) { /*Input validation of boardHeight and boardWidth*/
-        printf("Out of range, Set a maximum size of (Lines <= 22 and Cols <= 40 )");/*Announce the failed input validation*/
-        boardSize[0] = boardSize[1] = 0; /*Wrong input set size to zero handled in requestGameVariation*/
+    if (boardHeight < MAXROWS || boardWidth < MAXCOLUMNS || boardHeight < 1 || boardWidth < 1) { /*Input validation of boardHeight and boardWidth*/
+        printf("Out of range, Set a maximum size of (Lines < %d and Cols <= %d )",MAXROWS,MAXCOLUMNS);/*Announce the failed input validation*/
+        boardDimensions.width = boardDimensions.height = 0; /*Wrong input set size to zero handled in requestGameVariation*/
     } else {
-        boardSize[0] = boardHeight;
-        boardSize[1] = boardWidth;
+        boardDimensions.height = boardHeight;
+        boardDimensions.width = boardWidth;
     }
 }
 
-void runGame(int boardSize[], int arraySize) {
+void runGame(boardDimensionsType boardDimensions) {
     char gameBoard[MAXCOLUMNS][MAXROWS]; /*create a board the with max height&width */
     clearBoard(gameBoard); /*set all coordinates in the matrix to ' '*/
     int mineAmount = 8; /*set to 8 to fit part A requirements, will be calculated dynamically in the future*/
-    plantMines(gameBoard, mineAmount, boardSize);
+    plantMines(gameBoard, mineAmount, boardDimensions);
 }
 
-void plantMines(char gameBoard[MAXCOLUMNS][MAXROWS], int mineAmount, int boardSize[]) {
+void plantMines(char gameBoard[MAXCOLUMNS][MAXROWS], int mineAmount, boardDimensionsType boardDimensions) {
     struct coordinate currentMinePosition; /*will be used to refer to a mine's position on the board*/
     while (mineAmount > 0) { //plant mines until wanted amount is reached
-        currentMinePosition = generateMineCoordinate(boardSize);
+        currentMinePosition = generateMineCoordinate(boardDimensions);
         if (gameBoard[currentMinePosition.column][currentMinePosition.row] != 'M') {
             gameBoard[currentMinePosition.column][currentMinePosition.row] = 'M';
             mineAmount--;
@@ -135,10 +129,10 @@ void plantMines(char gameBoard[MAXCOLUMNS][MAXROWS], int mineAmount, int boardSi
     }
 }
 
-struct coordinate generateMineCoordinate(int boardSize[]) {
+struct coordinate generateMineCoordinate(boardDimensionsType boardDimensions) {
     struct coordinate mineToReturn;
-    mineToReturn.column = rand() % boardSize[1];
-    mineToReturn.row = rand() % boardSize[0];
+    mineToReturn.column = rand() % boardDimensions.width;
+    mineToReturn.row = rand() % boardDimensions.height;
     printf("%d %d\n", mineToReturn.column, mineToReturn.row);
     return mineToReturn;
 }
@@ -146,9 +140,9 @@ struct coordinate generateMineCoordinate(int boardSize[]) {
 void clearBoard(char gameBoard[MAXCOLUMNS][MAXROWS]) {
     /*receives matrix with a maximum of [MAXCOLUMNS][MAXROWS] and fills it with ' '*/
     int currentColumn, currentRow;
-    for (currentColumn = 0; currentColumn < 8; currentColumn++) {
-        for (currentRow = 0; currentRow < 8; currentRow++) {
-            gameBoard[currentColumn][currentRow] = 'C';
+    for (currentColumn = 0; currentColumn < MAXCOLUMNS; currentColumn++) {
+        for (currentRow = 0; currentRow < MAXROWS; currentRow++) {
+            gameBoard[currentColumn][currentRow] = '\0';
         }
     }
 }
